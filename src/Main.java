bf3a898a90a39ab4daa58ee47a39b71f063fb945
Dustin Derryberry <dustin.e.derryberry@gmail.com>
2026-07-05 14:11:12 -0500

    Update Main.java to test Swing UI usage

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+           JFrame frame = new JFrame("SE450 Project - Swing Test");
+           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           frame.setSize(300,200);
+
+           JLabel label = new JLabel("Hello, World!", SwingConstants.CENTER);
+           frame.add(label);
+
+           frame.setLocationRelativeTo(null);
+           frame.setVisible(true);
+        });
     }
 
 }
